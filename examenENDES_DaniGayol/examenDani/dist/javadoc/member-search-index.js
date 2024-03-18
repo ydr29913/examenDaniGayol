15504ed5bf08654b0ba0fc4c08fc85904393e818
ydr29913 <159024163+ydr29913@users.noreply.github.com>
2024-03-18 13:20:11 +0100

    Add files via upload
    Subir repositorio local a remoto

---
+++ examenENDES_DaniGayol/examenDani/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"miexamen","c":"Claseexamen","l":"Claseexamen()","u":"%3Cinit%3E()"},{"p":"miexamen","c":"Claseexamen","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ examenENDES_DaniGayol/examenDani/src/miexamen/Claseexamen.java
@@ -0,0 +1,35 @@
+package miexamen;
+
+/**
+ * Esta clase sirve para lanzar un texto por pantalla
+ * @author Dani Gayol
+ * @version 1.0.0
+ * @since 18/03/2024
+ */
+public class Claseexamen {
+    public static void main(String[] args) {
+        
+        System.out.println("Estoy haciendo el examen de la UT3");
+        
+        /**
+         * @return
+         * <!DOCTYPE html>
+         * <html lang="en">
+         * <head>
+         *  <meta charset="UTF-8">
+         *  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         *  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         *  <title>Document</title>
+         * 
+         * </head>
+         * <body>
+         *  <h1>Entornos de desarrolo UT3</h1>
+         *  
+         *  <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p>
+         *  
+         *  <p><a href="https://www.youtube.com/playlist?list=PLU8oAIHdN5BlyaPFiNQcVOxDqyOeR35aU" target="_blank">Curso de Git y github de píldoras informáticas</a></p>
+         * </body>
+         * </html>
+         */
+    }
+}
